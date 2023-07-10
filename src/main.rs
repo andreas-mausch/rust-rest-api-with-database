fb403fd0dd6fa75fc6b9f2e559775484b96b9140
neonew <neonew@gmail.com>
2023-07-10 15:12:30 +0200

    Use clap for CLI argument passing and iron as http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+use iron::{Iron, IronResult, Request, Response, status};
+
+/// Basic HTTP REST API Service
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Port to listen on
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+}
+
+fn hello(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello, world!")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    Iron::new(hello).http(("127.0.0.1", args.port)).unwrap();
 }
